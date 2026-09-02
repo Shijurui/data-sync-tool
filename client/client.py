a8301375dbbad7614ef54f1ef167d5fb84a1ce71
Shijurui <purplesparkle@keemail.me>
2026-09-02 22:37:51 +1000

    Removed unused import

--- client/client.py
+++ client/client.py
@@ -11,12 +11,11 @@ I have chosen to write a code snippet for the change detectiong as it deals with
 
 import sqlite3
 import os
-import datetime
 from hashlib import md5
 
 DATABASE_NAME = "files.db"
 SCRIPT_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
-MONITORED_DIRS = [os.path.join(SCRIPT_FILE_PATH.rsplit(os.path.sep, 1)[0], 'test_dir')]
+MONITORED_DIRS = [os.path.join(SCRIPT_FILE_PATH.rsplit(os.path.sep, 1)[0], "test_dir")]
 BUF_SIZE = 65536
 
 # Create/Connect to db
@@ -28,14 +27,15 @@ for table in tables_found:
     table_names.append(table[0])
 
 # Create tables
-if 'files' not in table_names:
-    cur.execute("CREATE TABLE files(file_path, file_hash, time_last_change, time_last_successful_sync, to_sync)")
-if 'last_run' not in table_names:
+if "files" not in table_names:
+    cur.execute(
+        "CREATE TABLE files(file_path, file_hash, time_last_change, time_last_successful_sync, to_sync)"
+    )
+if "last_run" not in table_names:
     cur.execute("CREATE TABLE last_run(time_last_run)")
 
 for path in MONITORED_DIRS:
     for file in os.listdir(path):
-
         # Find if file already exists in database
         file_exists = False
         previously_found_files = cur.execute("SELECT file_path FROM files")
@@ -51,14 +51,16 @@ for path in MONITORED_DIRS:
         if file_exists:
             # Check if file has been modified since last scan
             # This is a quicker way to check instead of comparing hashes for large files. It could be best to compare both time and hashes if there is a chance for the OS to give the wrong modified time.
-            db_last_modified_time = cur.execute(f"SELECT time_last_change FROM files WHERE file_path = '{file_path}'").fetchone()[0]
+            db_last_modified_time = cur.execute(
+                f"SELECT time_last_change FROM files WHERE file_path = '{file_path}'"
+            ).fetchone()[0]
             if last_modified_time == db_last_modified_time:
                 # File has not been modified
                 continue
 
         # Get md5 hash of file
         raw_file_hash = md5()
-        with open(file_path, 'rb') as f:
+        with open(file_path, "rb") as f:
             while True:
                 data = f.read(BUF_SIZE)
                 if not data:
@@ -68,10 +70,14 @@ for path in MONITORED_DIRS:
 
         # Save to database
         if file_exists == False:
-            cur.execute(f"INSERT INTO files VALUES ('{file_path}', '{file_hash}', '{last_modified_time}', 'NULL', 'Y')")
+            cur.execute(
+                f"INSERT INTO files VALUES ('{file_path}', '{file_hash}', '{last_modified_time}', 'NULL', 'Y')"
+            )
         else:
             # File has been modified, save to database
-            cur.execute(f"UPDATE files SET file_hash = '{file_hash}', time_last_change = '{last_modified_time}', to_sync = 'Y' WHERE file_path = '{file_path}'")
+            cur.execute(
+                f"UPDATE files SET file_hash = '{file_hash}', time_last_change = '{last_modified_time}', to_sync = 'Y' WHERE file_path = '{file_path}'"
+            )
         con.commit()
 
 res = cur.execute("SELECT * FROM files")
@@ -79,4 +85,3 @@ for row in res.fetchall():
     print(row)
 
 con.close()
-
